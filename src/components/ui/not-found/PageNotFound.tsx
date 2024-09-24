00fdb2259c0424e1279f77de83eb22f67080cd0d
Omar Villanueva <jose.villanueva2888@alumnos.udg.mx>
2024-09-24 11:45:33 -0600

    Added not found page for categories

---
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link"
+import { PiArrowUDownLeftFill } from "react-icons/pi";
+import { titleFont } from "@/config/fonts"
+import Image from "next/image";
+
+export const PageNotFound = () => {
+    return (
+        <div className="flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle">
+            
+            <div className="text-center px-5 mx-5">
+                <h2 className={` ${titleFont.className} antialiased text-9xl`}>404</h2>
+                <p className="font-semibold text-xl">Whoops! Pagina no encontrada</p>
+                <p>
+                    <Link className="flex flex-row justify-center font-normal hover:underline transition-all" href="/">
+                        <p className="mx-2">Regresar al inicio</p>
+                        <PiArrowUDownLeftFill className="h-7"/>
+                    </Link>
+                </p>
+            </div>
+
+            <div className="px-5 mx-5">
+                <Image
+                    src="/imgs/starman_750x750.png"
+                    alt="Starman"
+                    className="p-5 sm:p-0"
+                    width={550}
+                    height={550}
+                />
+            </div>
+        </div>
+    )
+}
