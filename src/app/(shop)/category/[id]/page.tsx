73f3113bc95113cf41878afbf75c4f8daf33f575
Omar Villanueva <jose.villanueva2888@alumnos.udg.mx>
2024-09-26 14:32:25 -0600

    Added item page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,19 +1,37 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
     params: {
-        id: string;
+        id: Category;
     }
 }
+const products = initialData.products;
 
 export default function({params} : Props) {
     const {id} = params;
-    if(id === "kids") {
+    // if(id === "kids") {
+    //     notFound();
+    // }
+
+    const myProducts = products.filter( prod => id === prod.gender)
+    if (myProducts.length === 0) {
         notFound();
     }
+
+    const labels: Record<Category, string> = {
+        'men': 'Hombres',
+        'women': 'Mujeres',
+        'kid': 'Niños',
+        'unisex': 'Todos',
+    }
+
     return (
-        <div>
-            <h1>Category Page</h1>
-        </div>
+        <>
+            <Title title={`Artículos para ${labels[id]}`} subtitle={`${myProducts.length} productos`} classname="mb-2"/>
+            <ProductGrid products={myProducts}/>
+        </>
     );
 }
