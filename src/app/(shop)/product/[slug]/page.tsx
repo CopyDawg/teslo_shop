788323030908908854674183ed5df68030d1a11a
Omar Villanueva <jose.villanueva2888@alumnos.udg.mx>
2024-09-27 13:31:23 -0600

    Finished product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -21,7 +22,19 @@ export default function({params} : Props) {
         <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
             {/* Slideshow */}
             <div className="col-span-1 md:col-span-2">
-                He
+                {/* Mobile slideshow */}
+                <ProductMobileSlideshow
+                title={product.title}
+                images={product.images}
+                className="block md:hidden"
+                />
+
+                {/* Desktop slideshow */}
+                <ProductSlideshow
+                title={product.title}
+                images={product.images}
+                className="hidden md:block"
+                />
             </div>
 
             {/* Detalles */}
@@ -29,13 +42,13 @@ export default function({params} : Props) {
                 <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
                     {product.title}
                 </h1>
-                <p className="text-lg mb-5">{product.price}</p>
+                <p className="text-lg mb-5">${product.price}</p>
 
                 {/* Selector de tallas */}
-
+                <SizeSelector selectedSize={product.sizes[0]} availableSizes={product.sizes}/>
 
                 {/* Selector de cantidad */}
-
+                <QuantitySelector quantity={2}/>
 
                 {/* Boton add to cart */}
                 <button className="btn-primary my-5">Agregar al carrito</button>
