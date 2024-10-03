c59a0c0852ecdd47e8090d0d0541111baf3856bd
Omar Villanueva <jose.villanueva2888@alumnos.udg.mx>
2024-10-03 13:13:30 -0600

    Finished frontend

--- src/components/ui/sidebar/SidebarButton.tsx
+++ src/components/ui/sidebar/SidebarButton.tsx
@@ -11,9 +11,9 @@ export const SidebarButton = ({href, text, Icon} : Props) => {
     return (
         <Link
             href={href}
-            className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+            className="flex items-center mt-7 p-2 hover:bg-gray-100 rounded transition-all"
         >
-            <Icon size={30} />
+            <Icon size={27} />
             <span className="ml-3 text-xl">{text}</span>
         </Link>
     )
